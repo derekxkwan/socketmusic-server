53b157a5f90638b67191a16b49cb672f1004be62
Derek Kwan <derek.x.kwan@gmail.com>
2018-11-21 14:47:44 -0800

    first stable-ish, add osc/ws routing

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# socketmusic-server
-server for the socketmusic series of project
+# socketmusic-server v 0.01
+server for the socketmusic series of projects (such as (socketMusic:wireless)[https://derekxkwan.com/musicart/socketMusic.html])
 
 ## about
 abstracting out the server from my previous socketmusic:wireless project and moving to a new build system of **ClojureScript** compiled by **shadow-cljs** to run on **node.js**
@@ -17,13 +17,23 @@ abstracting out the server from my previous socketmusic:wireless project and mov
 - outgoing osc messages are on port `11111`
 - website is hosted on port `3000`
 - the root directory of the hosted website is passed as the **first arg**
-  - defaults to root of server app
+  - defaults to `default-page/` folder
 - running the app: `node socketmusic.js [webpage dir]`
+
+## osc-messages
+format for osc messages sent to the osc server
+- `/debug args` prints the args to the console
+- `/client-list` sends out base64 client list on osc output port on address `/client-list`
+- `/num-clients` sends the number of clients on osc output port on address `/num-clients`
+- `/ws/all [args]` broadcasts array of args to all clients
+- `/ws/client-idx [client-idx] [args]` - sends array of args to client at given index of client-list
+- `/ws/client [client] [args]` - sends array of args to specified (in base64) client
+- `/ws/rand-client [args]` - sends array of args to random client
 
 ## build
 - compile: `shadow-cljs compile app`
 - watch: `shadow-cljs watch app`
 - build: `shadow-cljs build app`
-
-## status
-under construction...
+- 
+## license
+gpl v 3
--- src/socketmusic/core.cljs
+++ src/socketmusic/core.cljs
@@ -13,28 +13,84 @@
 (defonce osc-server (atom nil))
 (defonce osc-client (atom nil))
 
+(def clients (atom {}))
+
+(defn provide-client-list []
+  (mapv name (keys @clients)))
+
+(defn provide-num-clients []
+  (count (keys @clients)))
+
+(defn get-client-by-idx [idx]
+  (get (provide-client-list) idx))
+
+(defn send-client-list []
+  (.send @osc-client "/client-list" (apply array (provide-client-list))))
+
+(defn send-num-clients []
+  (.send @osc-client "/num-clients" (provide-num-clients)))
+
 (defn start-app []
   (let [app (ep)]
     (if (not (nil? page-dir))
       (do (.use app (ep.static. page-dir))
           (println "hosting webpage located at" page-dir))
-      (do (.use app (ep.static. (str js/__dirname "/")))
-          (println "hosting webpage located at" (str js/__dirname "/")))
+      (do (.use app (ep.static. (str js/__dirname "/default-page/")))
+          (println "hosting webpage located at" (str js/__dirname "/default-page/")))
       )
     (.listen app server-port (fn [] (println "starting app server on port" (str server-port))))
     )
   )
 
+(defn socket-callbacks [socket]
+  (.on socket "connection"
+       (fn [client]
+         (swap! clients assoc (keyword (.-id client)) client)
+         (.on client "disconnect"
+              (fn []
+                (swap! clients dissoc (keyword (.-id client)))
+                )
+              ))
+       )
+  )
+         
+
+(defn ws-broadcast [args]
+  (println (str "broadcasting " args))
+  (let [address (first args)
+        cur-args (apply array (rest args))]
+    (.emit @io address cur-args)
+    )
+  )
+  
+
+(defn ws-send [target args]
+  ;(println (str "sending " args " to " target))
+  (let [address (first args)
+        cur-args (apply array (rest args))]
+    (.emit (.to @io target) address cur-args)
+    )
+  )
+
 (defn start-ws [cur-http]
-  (println "starting websockets")
-  (sio cur-http)
+  ;(println "starting websockets")
+  (let [ws-server (sio cur-http)]
+    (socket-callbacks ws-server)
+    ws-server)
   )
 
 (defn osc-router [msg rinfo]
+  ;(println msg)
   (let [address (first msg)
-        args (vec (rest msg))]
+        args (rest msg)]
     (cond
-      (= address "/log") (println (array args))
+      (= address "/debug") (println args)
+      (= address "/client-list") (send-client-list)
+      (= address "/num-clients") (send-num-clients)
+      (= address "/ws/all") (ws-broadcast args)
+      (= address "/ws/client-idx") (ws-send (get-client-by-idx (first args)) (rest args))
+      (= address "/ws/client") (ws-send (first args) (rest args))
+      (= address "/ws/rand-client") (ws-send (get-client-by-idx (rand-int (provide-num-clients))) args)
       :else nil)
     )
   )
